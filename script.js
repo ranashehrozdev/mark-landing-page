6cd2c27bb4b59c3a63837ecb65ab63958acd3e3c
Rana Shehroz <66749505+ranashehrozdev@users.noreply.github.com>
2022-12-29 17:02:15 +0500

    updated

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-$(document).ready(function() {
-    // Toggle menu on click
-    $("#menu-toggler").click(function() {
-      toggleBodyClass("menu-active");
-    });
-   function toggleBodyClass(className) {
-      document.body.classList.toggle(className);
-    }
-  
-   
-   });
+$(document).ready(function () {
+  // Toggle menu on click
+  $("#menu-toggler").click(function () {
+    toggleBodyClass("menu-active");
+  });
+
+  function toggleBodyClass(className) {
+    document.body.classList.toggle(className);
+  }
+});
+
+
+function splitScroll() {
+  const controller = new ScrollMagic.Controller();
+
+  new ScrollMagic.Scene({
+      duration: "950%",
+      triggerElement: ".abbout__title",
+      triggerHook: 0
+    })
+
+    .setPin(".abbout__title")
+    // .addIndicators()
+    .addTo(controller);
+}
+
+splitScroll();
